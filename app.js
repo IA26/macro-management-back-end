91751905dcadbe974af0c00a86062aaa7cdd0a43
Isaac <isaac1180@aol.com>
2021-01-07 16:34:45 -0500

    added home page and port to listen to

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const express = require('express');
 
 const app = express(); 
+
+//ROUTES
+
+app.get('/', (req, res) => {
+    res.send("Home page");
+});
+
+
+
+
+
+
+// LISTENING 
+app.listen(8000);
